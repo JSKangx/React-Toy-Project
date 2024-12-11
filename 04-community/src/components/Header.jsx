d4d5f26100d2a5ae77837fa44ab271422c591785
Jinsu <injnamek@gmail.com>
2024-12-11 21:46:37 +0900

    custom axios instance 생성

--- 04-community/src/components/Header.jsx
+++ 04-community/src/components/Header.jsx
@@ -1,15 +1,41 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="flex justify-between">
-      <Link to="/" className="bg-orange-300 p-3">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? "bg-orange-300 p-3" : "bg-slate-200 p-3"
+        }
+      >
         멋사컴
-      </Link>
-      <div className="flex gap-4 *:bg-orange-300 *:p-3">
-        <Link to="/info">정보 공유</Link>
-        <Link to="/free">자유 게시판</Link>
-        <Link to="/qna">질문 게시판</Link>
+      </NavLink>
+      <div className="flex gap-4">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "bg-orange-300 p-3" : "bg-slate-200 p-3"
+          }
+          to="/info"
+        >
+          정보 공유
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "bg-orange-300 p-3" : "bg-slate-200 p-3"
+          }
+          to="/free"
+        >
+          자유 게시판
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "bg-orange-300 p-3" : "bg-slate-200 p-3"
+          }
+          to="/qna"
+        >
+          질문 게시판
+        </NavLink>
       </div>
       <div className="flex gap-4 *:bg-orange-300 *:p-3">
         <Link to="/users/login">로그인</Link>
---
+++ 04-community/src/hooks/useAxiosInstance.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export default function useAxiosInstance() {
+  // custom axios instance 생성
+  const instance = axios.create({
+    baseURL: "https:/11.fesp.shop",
+    timeout: 1000 * 15,
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json",
+      "client-id": "00-board",
+    },
+  });
+
+  // (1) 요청 인터셉터 추가
+  // 인자로 전달되는 config는 요청 환경 설정 객체
+  instance.interceptors.request.use((config) => {
+    // 요청 Params 설정
+    config.params = {
+      delay: 500, // 딜레이 0.5초
+      ...config.params, // 나머지 속성 값은 그대로 유지
+    };
+
+    return config; // 변경된 config 객체 반환
+  });
+
+  // (2) 응답 인터셉터 추가
+  instance.interceptors.response.use(
+    (response) => {
+      // 제대로된 응답이 왔을 때 실행할 코드 블록
+      return response;
+    },
+    (error) => {
+      // 에러가 발생했을 때 실행할 코드 블록
+      console.error(error); // 콘솔에 에러 메시지 추가
+
+      // 에러 객체 반환. 이 instance를 사용하는 요청의 catch 블록의 인수로 전달됨
+      return Promise.reject(error);
+    }
+  );
+}
